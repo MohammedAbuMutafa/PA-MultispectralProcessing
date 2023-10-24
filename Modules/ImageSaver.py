6df22c34db900320f40fa292d88f21e6f68d21c1
Saif <saif_kh31@outlook.com>
2023-10-24 23:02:21 +0400

    sync

--- Modules/ImageSaver.py
+++ Modules/ImageSaver.py
@@ -11,7 +11,7 @@ class ImageSaver():
         self.directory_manager.create_session_dirs()
 
     def save_image(self, img, image_details: IncomingMessageDTO, processing_type: MultiSpectralEnum, use_color_bar=False):
-        fig = plt.figure(frameon=False)
+        fig = plt.figure(frameon=False, figsize=(7.50, 7.50), dpi=100)
         ax = plt.Axes(fig, [0., 0., 1., 1.])
         ax.set_axis_off()
         fig.add_axes(ax)
@@ -21,5 +21,5 @@ class ImageSaver():
         output_dir = self.directory_manager.session_dir
         file = os.path.join(output_dir, processing_type.name,
                             image_details.fileName)
-        fig.savefig(file, bbox_inches='tight', pad_inches=0.0)
+        fig.savefig(file, bbox_inches='tight', pad_inches=0.0, dpi=100)
         plt.close(fig)
